2860cc383edecba17a0a3f6e20fc3279915c8d8e
kazikaif <130026107+kazikaif@users.noreply.github.com>
2025-02-22 12:14:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+// script.js
+const search = document.querySelector('.search');
+const input = document.querySelector('.input');
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', () => {
+    search.classList.toggle('active');
+    input.focus();
+});
